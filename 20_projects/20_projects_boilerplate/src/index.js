2ca55734478e4a3edfd68aca2ab7f74663510846
Bharati Subramanian <bharatisharada@gmail.com>
2021-08-12 14:56:52 +0530

    completed project 20

--- 20_projects/20_projects_boilerplate/src/index.js
+++ 20_projects/20_projects_boilerplate/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom'
 import axios from 'axios'
 
 export function Cat (props) {
-  console.log(props);
-  let catImage = props.cat.image.url;
+  console.log(props.cat.image.url);
+  let catImage = props.cat.image["url"];
   return (
     <>
       <div className="cat-about" style= {{
@@ -24,12 +24,20 @@ export function Cat (props) {
   // return <p>{catImage}</p>
   }
 
-function CalculateAverageWeight(props) {
-    console.log(props);
-    // let weight = Object.values(props.weight.weight);
-    // console.log(weight);
-
+class CalculateAverageWeight extends React.Component  {  
+    constructor(props) {
+    super(props);
+    this.state = {
+      weight: props.weight
+    }
   }
+  render() {
+    console.log(this.props);
+    return(
+      console.log(this.state.weight.split(" - "))
+    )
+  }
+}
 
 class App extends Component {
   state = {
@@ -41,20 +49,49 @@ class App extends Component {
     this.fetchCatData();
   }
   fetchCatData = async () => {
-    const url = 'https://api.thecatapi.com/v1/breeds?limit=10'
+    const url = 'https://api.thecatapi.com/v1/breeds/?limit=10';
     try {
       const response = await axios.get(url)
       const data = await response.data
       this.setState({
         data,
+       
       })
     } catch (error) {
       console.log(error)
     }
   }
 
+  calculateAverage() {
+    let weight = this.state.data.map(cat => {
+      let min = Number(cat.weight.metric.split(" - ")[0]);
+      let max = Number(cat.weight.metric.split(" - ")[1]);
+
+      return min+max/2;
+    });
+
+    console.log(weight);
+
+    let totalWeight = 0;
+    weight.forEach(w => {
+      totalWeight+=w;
+    })
+  return totalWeight/weight.length;
+  } 
   
-  
+  calculateLifeSpan() {
+    let lifeSpan = this.state.data.map(cat => {
+      let min = Number(cat.life_span.split(" - ")[0]);
+      let max = Number(cat.life_span.split(" - ")[1]);
+
+      return min+max/2;
+    })
+    let totalLifeSpan = 0;
+    lifeSpan.forEach(l => {
+      totalLifeSpan+=l;
+    })
+  return totalLifeSpan/lifeSpan.length; 
+  }
   
   render() {
 
@@ -62,19 +99,24 @@ class App extends Component {
     let weightProps = {weight: weight};
     console.log(weight);
     return (
-      <div className='App'>
+      <div className='App' style={{textAlign: 'center'}}>
         <h1>React Component Life Cycle</h1>
+          <div style={{textAlign: "Center"}}>
+            <h2>{this.calculateAverage()}</h2>
+            <h2>{this.calculateLifeSpan()}</h2>
+          </div>
         <h1>Calling API</h1>
         <div className='cat-wrapper' style={{
             maxWidth: "900px",
             margin: "2rem auto"}}>
-          
+            
+
             {this.state.data.map((cat) => {
               return <Cat cat={cat}></Cat>
             })}
-        </div>
 
-        <CalculateAverageWeight weight = {weightProps} />
+            
+        </div>
       </div>
     )
   }
